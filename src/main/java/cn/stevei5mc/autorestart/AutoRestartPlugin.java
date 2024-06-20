50b45d3b50b7881f04c8993e26ba7c00e34ae89a
stevei5mc <2978383306@qq.com>
2024-06-20 23:16:42 +0800

    支持多语言

--- src/main/java/cn/stevei5mc/autorestart/AutoRestartPlugin.java
+++ src/main/java/cn/stevei5mc/autorestart/AutoRestartPlugin.java
@@ -5,35 +5,52 @@ import cn.nukkit.scheduler.Task;
 import cn.nukkit.utils.Config;
 import cn.nukkit.Player;
 import cn.nukkit.level.Sound;
-import cn.nukkit.network.protocol.PlaySoundPacket;
+import cn.lanink.gamecore.utils.Language;
+import cn.nukkit.command.CommandSender;
 
-import java.util.concurrent.TimeUnit;
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.*;
 
 public class AutoRestartPlugin extends PluginBase {
     private int restartTime = 2; // 设置重启前的延迟时间（单位：分钟）
+    private Language language;
+    private String defaultLanguage;
+    private final HashMap<String, Language> languageMap = new HashMap<>();
+    private Config config;
+
+    public Config getConfig() {
+        return this.config;
+    }
 
     public void onLoad() {
         saveDefaultConfig();
         saveLanguageFile();
+        this.config = new Config(this.getDataFolder() + "/config.yml", Config.YAML);
     }
 
     public void onEnable() {
-        Config config = this.getConfig();
-        restartTime = config.getInt("restart_time", 2);// 设置重启前的延迟时间（单位：分钟）
-        scheduleRestart();// 当插件被启用时，计划自动重启任务
-        getLogger().info("自动重启插件已启用，将在 §a" + restartTime + " §f分钟后重启服务器。");
-        getLogger().warning("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
-        getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/AutoRestart");
+        if (this.getServer().getPluginManager().getPlugin("MemoriesOfTime-GameCore") != null) {
+            restartTime = config.getInt("restart_time", 2);// 设置重启前的延迟时间（单位：分钟）
+            loadLanguage();
+            scheduleRestart();// 当插件被启用时，计划自动重启任务
+            getLogger().info(this.getLang().translateString("server_msg_restart_time", restartTime));
+            getLogger().warning("§c警告! §c本插件为免费且开源的一款插件，如果你是付费获取到的那么你就被骗了");
+            getLogger().info("§a开源链接和使用方法: §bhttps://github.com/stevei5mc/AutoRestart");
+        } else {
+            //不存在作为卸载该插件
+            this.getLogger().warning("§c未检测到前置插件§aTips§c，请安装§aMemoriesOfTime-GameCore§c再试!!!");
+            this.getLogger().warning("§b下载地址: §ehttps://motci.cn/job/GameCore/");
+            this.onDisable();
+        }
+    }
+
+    public void onDisable() {
+        this.getLogger().info("已停止运行，感谢你的使用");
     }
 
     private void scheduleRestart() {
-        Config config = this.getConfig();
         // 使用 Nukkit 的定时任务系统来计划重启
         getServer().getScheduler().scheduleRepeatingTask(this, new Task() {
             int timeLeft = restartTime * 60; // 将分钟转换为秒
-
             @Override
             public void onRun(int currentTick) {
                 timeLeft--;// 每秒减少时间
@@ -41,27 +58,23 @@ public class AutoRestartPlugin extends PluginBase {
                 int tipTime = config.getInt("tips_time", 30);
                 if (timeLeft <= tipTime) {
                     for (Player player : getServer().getOnlinePlayers().values()) {
-                        String title = "§c即将重启";
-                        String subtitle = "§e本分支服即将在 §6{seconds} §e秒后重启"; 
                         if (config.getBoolean("show_title",true)) {
-                            player.sendTitle(title.replace("{seconds}",String.valueOf(timeLeft)), subtitle.replace("{seconds}",String.valueOf(timeLeft)), 0, 20, 0);
+                            player.sendTitle((getLang(player).translateString("restart_msg_title", timeLeft)), (getLang(player).translateString("restart_msg_subtitle", timeLeft)), 0, 20, 0);
                         }
                         if (config.getBoolean("show_tip",true)) {
-                            player.sendTip(subtitle.replace("{seconds}",String.valueOf(timeLeft)));
+                            player.sendTip(getLang(player).translateString("restrat_msg_tip", timeLeft));
                         }
                         if (config.getBoolean("play_sound",true)) {
-                            //copy https://github.com/glorydark/CustomForm/blob/main/src/main/java/glorydark/nukkit/customform/scriptForms/data/SoundData.java
+                            //参考和使用部分代码 https://github.com/glorydark/CustomForm/blob/main/src/main/java/glorydark/nukkit/customform/scriptForms/data/SoundData.java
                             // 读取配置中音效的设置
                             String soundName = config.getString("sound.name","random.toast");
                             float volume = (float) config.getDouble("sound.volume",1.0);
                             float pitch = (float) config.getDouble("sound.pitch",1.0);
-                            // 获取音效对象
-                            Optional<Sound> find = Arrays.stream(Sound.values()).filter(get -> get.getSound().equals(soundName)).findAny();
+                            Optional<Sound> find = Arrays.stream(Sound.values()).filter(get -> get.getSound().equals(soundName)).findAny();// 获取音效对象
                             Sound sound = find.orElse(null);
                             // 检查音效是否存在
                             if (sound != null) {
-                                // 播放音效给玩家
-                                player.getLevel().addSound(player.getLocation(), sound, volume, pitch);
+                                player.getLevel().addSound(player.getLocation(), sound, volume, pitch);// 播放音效给玩家
                             }
                         }
                     }
@@ -71,10 +84,9 @@ public class AutoRestartPlugin extends PluginBase {
                     this.cancel();
                     if (config.getBoolean("kick_player",true)) {
                         for (Player player : getServer().getOnlinePlayers().values()) {
-                            player.kick("§e服务器正在重启\n稍后再会", false);
+                            player.kick((getLang(player).translateString("kick_player_msg")), false);
                         }
                     }
-                    getLogger().info("自动重启服务器...");
                     getServer().shutdown(); // 关闭服务器  注意：这里不会自动重启，你需要配置服务器管理工具或脚本来自动重启服务器进程
                 }
             }
@@ -84,8 +96,40 @@ public class AutoRestartPlugin extends PluginBase {
     private void saveLanguageFile() {
         String[] langList = new String[]{"zh_CN", "zh_TW","en_US"};
         for(String lang: langList){
-            saveResource("language/"+lang+".yml", "language/"+lang+".yml",false);
-            getLogger().info("已加载语言文件 "+lang+".yml");
+            saveResource("language/"+lang+".properties",false);
+            getLogger().info("已加载语言文件 "+lang+".properties");
         }
+    }
+    //使用https://github.com/MemoriesOfTime/CrystalWars/blob/master/src/main/java/cn/lanink/crystalwars/CrystalWars.java
+    private void loadLanguage() {
+        List<String> languages = Arrays.asList("zh_CN", "zh_TW","en_US");
+        this.defaultLanguage = this.config.getString("default_language", "zh_CN");
+        if (!languages.contains(this.defaultLanguage)) {
+            this.getLogger().error("Language" + this.defaultLanguage + "Not supported, will load Chinese!");
+            this.defaultLanguage = "zh_CN";
+        }
+        for (String language : languages) {
+            Config languageConfig = new Config(Config.PROPERTIES);
+            languageConfig.load(this.getResource("language/" + language + ".properties"));
+            this.languageMap.put(language, new Language(languageConfig));
+        }
+        this.getLogger().info(this.getLang().translateString("plugin_language"));
+    }
+    //同上
+    public Language getLang() {
+        return this.getLang(null);
+    }
+    //同上
+    public Language getLang(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String playerLanguage = player.getLoginChainData().getLanguageCode();
+            if (!this.languageMap.containsKey(playerLanguage)) {
+                playerLanguage = this.defaultLanguage;
+                getLogger().info("zhcn");
+            }
+            return this.languageMap.get(playerLanguage);
+        }
+        return this.languageMap.get(this.defaultLanguage);
     }
 }
