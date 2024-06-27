6c2a9ef3b638f4a0a0115b4a53e6b9c898adb768
stevei5mc <2978383306@qq.com>
2024-06-27 20:08:01 +0800

    将相同的内容移到一个工具类

--- src/main/java/cn/stevei5mc/autorestart/tasks/DispatchRestartTask.java
+++ src/main/java/cn/stevei5mc/autorestart/tasks/DispatchRestartTask.java
@@ -1,12 +1,10 @@
 package cn.stevei5mc.autorestart.tasks;
 
 import cn.nukkit.scheduler.Task;
-import cn.nukkit.Player;
-import cn.nukkit.level.Sound;
 import cn.lanink.gamecore.utils.Language;
 import cn.stevei5mc.autorestart.AutoRestartPlugin;
+import cn.stevei5mc.autorestart.Utils;
 
-import java.util.*;
 
 public class DispatchRestartTask extends Task {
     protected AutoRestartPlugin main = AutoRestartPlugin.getInstance();
@@ -15,36 +13,10 @@ public class DispatchRestartTask extends Task {
     @Override
     public void onRun(int currentTick) {
         time--;// 每秒减少时间
-        for (Player player : main.getServer().getOnlinePlayers().values()) {
-            if (main.getConfig().getBoolean("show_title",true)) {
-                player.sendTitle((main.getLang(player).translateString("restart_msg_title", time)), (main.getLang(player).translateString("restart_msg_subtitle", time)), 0, 20, 0);
-            }
-            if (main.getConfig().getBoolean("show_tip",true)) {
-                player.sendTip(main.getLang(player).translateString("restrat_msg_tip", time));
-            }
-            if (main.getConfig().getBoolean("play_sound",true)) {
-                //参考和使用部分代码 https://github.com/glorydark/CustomForm/blob/main/src/main/java/glorydark/nukkit/customform/scriptForms/data/SoundData.java
-                // 读取配置中音效的设置
-                String soundName = main.getConfig().getString("sound.name","random.toast");
-                float volume = (float) main.getConfig().getDouble("sound.volume",1.0);
-                float pitch = (float) main.getConfig().getDouble("sound.pitch",1.0);
-                Optional<Sound> find = Arrays.stream(Sound.values()).filter(get -> get.getSound().equals(soundName)).findAny();// 获取音效对象
-                Sound sound = find.orElse(null);
-                // 检查音效是否存在
-                if (sound != null) {
-                    player.getLevel().addSound(player.getLocation(), sound, volume, pitch);// 播放音效给玩家
-                }
-            }
-        }
+        Utils.sendRestartMsg(time);
         // 如果时间到了，重启服务器
         if (time <= 0) {
-            this.cancel();
-            if (main.getConfig().getBoolean("kick_player",true)) {
-                for (Player player : main.getServer().getOnlinePlayers().values()) {
-                    player.kick((main.getLang(player).translateString("kick_player_msg")), false);
-                }
-            }
-            main.getServer().shutdown(); // 关闭服务器  注意：这里不会自动重启，你需要配置服务器管理工具或脚本来自动重启服务器进程
+            Utils.shutdownServer();
         }
     }
 }
