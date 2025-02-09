120fa180e79933157d027bcfb16d19e025f92723
stevei5mc <82391055+stevei5mc@users.noreply.github.com>
2025-02-09 12:47:58 +0800

    dev 1. 调整if判断 2. 调整对接Tips的变量在特定的条件下的显示内容

--- src/main/java/cn/stevei5mc/autorestart/utils/BaseUtils.java
+++ src/main/java/cn/stevei5mc/autorestart/utils/BaseUtils.java
@@ -14,7 +14,7 @@ public class BaseUtils {
     public static int getRestartTipTime() {
         int time = 30;
         int i = main.getConfig().getInt("tips_time", 30);
-        if (time != 0) {
+        if (i > 0) {
             time = i;
         }
         return time;
@@ -27,7 +27,7 @@ public class BaseUtils {
     public static int getRestartUseTime() {
         int time = 2;
         int i = main.getConfig().getInt("restart_time", 2);
-        if (i != 0) {
+        if (i > 0) {
             time = i;
         }
         return time;
@@ -35,32 +35,35 @@ public class BaseUtils {
 
     /**
      * 获取剩余时间
-     * @param player player
+     * @param player 传入player参数以实现多语言
      * @return remainder
     */
     public static String getRemainder(Player player) {
-        int time = RestartTask.getTime();
-        int hours = time / 3600;
-        int minutes = (time % 3600) / 60;
-        int seconds = time % 60;
         String hourUnit = main.getLang(player).translateString("time_unit_hour");
         String minuteUnit = main.getLang(player).translateString("time_unit_minutes");
         String secondUnit = main.getLang(player).translateString("time_unit_seconds");
-        String timee = "";
-        if (hours > 0) {
-            timee = String.valueOf(hours) + hourUnit + String.valueOf(minutes) + minuteUnit + String.valueOf(seconds) + secondUnit;
-        } else if (minutes > 0) {
-            timee = String.valueOf(minutes) + minuteUnit + String.valueOf(seconds) + secondUnit;
-        } else {
-            timee = String.valueOf(seconds) + secondUnit;
+        if (TasksUtils.getRestartTaskState() >= 1 && TasksUtils.getRestartTaskType() != 3) {
+            int time = RestartTask.getTime();
+            int hours = time / 3600;
+            int minutes = (time % 3600) / 60;
+            int seconds = time % 60;
+            String timee = "";
+            if (hours > 0) {
+                timee = hours + hourUnit + minutes + minuteUnit + seconds + secondUnit;
+            } else if (minutes > 0) {
+                timee = minutes + minuteUnit + seconds + secondUnit;
+            } else {
+                timee = seconds + secondUnit;
+            }
+            return main.getLang(player).translateString("variable_remainder",timee);
+        }else {
+            return "--"+secondUnit;
         }
-        String remainder = main.getLang(player).translateString("variable_remainder",timee);
-        return remainder;
     }
 
     /**
      * 获取重启任务名
-     * @param player Player 传入play参数以实现多语言
+     * @param player 传入player参数以实现多语言
      * @return Task name
     */
     public static String getRestartTaskName(Player player) {
